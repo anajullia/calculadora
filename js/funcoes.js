d1e556c7b305fcd91b851b3d59f0b1c34a310a23
meunome <meunome@meunome>
2022-11-17 20:01:03 -0300

    Área

--- js/funcoes.js
+++ js/funcoes.js
@@ -15,3 +15,66 @@ function Calculadora(){
     document.getElementById("mostrar-resultado").innerHTML = resultado;
     /* innerhtml é tipo document.write mas sem ter que reiniciar a página*/
 }
+
+function Potenciacao(){
+    num = document.getElementById("num").value;
+    expoente = document.getElementById("expoente").value;
+
+    if(expoente < 0){
+        resultado = ("<h3>O valor do expoente não pode ser menor que 0</h3>");
+    }
+    else{
+        resultado = eval(parseInt(Math.pow(num, expoente)));
+    }
+
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+function Raiz(){
+    num = document.getElementById("num").value;
+    raiz = document.getElementById("raiz").value;
+
+    if(num < 0){
+        resultado = ("<h3>O valor do radicando não pode ser menor que 0, pois não pertence aos reais</h3>");
+    }
+    else{
+        resultado = eval(Math.pow(parseInt(num), 1.0/ parseInt(raiz)));
+    }
+
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+function Area(){
+    b = document.getElementById("base").value
+    h = document.getElementById("altura").value
+
+    resultado = eval((b * h)/2);
+
+    document.getElementById("resultado").innerHTML = resultado + "cm²";
+}
+
+function Bhaskara(){
+    A = document.getElementById("A").value;
+    B = document.getElementById("B").value;
+    C = document.getElementById("C").value;
+
+    delta = eval(parseFloat((B * B) -4 * A * C));
+    document.getElementById("resultado").innerHTML = "delta = " + delta;
+
+
+    if(delta < 0){
+        document.getElementById("resultado1").innerHTML = "O valor do primeiro coeficiente (A) pode ser diferente de 0 ";
+    }
+    else if(delta == 0){
+        x1 = eval(parseFloat((-B + Math.sqrt(delta)) / (2 * A)));
+        document.getElementById("resultado1").innerHTML = "X1 e X2 = " + x1;
+
+
+    }else{
+        x1 = eval(parseFloat((-B + Math.sqrt(delta)) / (2 * A)));
+        x2 = eval(parseFloat((-B - Math.sqrt(delta)) / (2 * A)));
+        document.getElementById("resultado1").innerHTML = "X1 = " + x1;
+        document.getElementById("resultado2").innerHTML = "X2 = " + x2;
+        
+    }
+}
